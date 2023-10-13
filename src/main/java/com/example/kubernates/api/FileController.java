8ac8459d6003d00aa18ebebbbc6648c262656fbc
Ashish Garg <ashgarg@publicisgroupe.net>
2023-10-13 14:53:04 +0100

    Updated code

--- src/main/java/com/example/kubernates/api/FileController.java
+++ src/main/java/com/example/kubernates/api/FileController.java
@@ -1,5 +1,7 @@
 package com.example.kubernates.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
@@ -16,19 +18,20 @@ import static org.springframework.http.MediaType.MULTIPART_FORM_DATA_VALUE;
 
 @RestController
 public class FileController {
-    @PostMapping(value = "/file", consumes = {MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity<List<String>> uploadFile(@RequestPart(value = "file") MultipartFile file) throws IOException {
+    private final Logger LOGGER = LoggerFactory.getLogger(FileController.class);
+
+    @PostMapping(value = "/file", consumes = MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<String>> uploadAndExtractZipFile(@RequestPart MultipartFile file) {
         List<String> zipContent = new ArrayList<>();
-        String originalFileName = file.getOriginalFilename();
-        ZipInputStream zis = new ZipInputStream(file.getInputStream());
-        ZipEntry zipEntry = zis.getNextEntry();
-        while (zipEntry != null) {
-            System.out.println(zipEntry.getName());
-            zipContent.add(zipEntry.getName());
-            zipEntry = zis.getNextEntry();
+        try (ZipInputStream stream = new ZipInputStream(file.getInputStream())) {
+            LOGGER.info("Zip file: " + file.getOriginalFilename() + " has been opened");
+            ZipEntry entry;
+            while ((entry = stream.getNextEntry()) != null) {
+                zipContent.add(entry.getName());
+            }
+        } catch (IOException ex) {
+            LOGGER.info("Exception reading zip", ex);
         }
-        zis.closeEntry();
-        zis.close();
         return ResponseEntity.ok(zipContent);
     }
 }
